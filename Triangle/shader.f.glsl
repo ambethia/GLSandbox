9d10259b6dbf2193dd25b45eddf78db97d8fe8a4
Jason L Perry <jasper@ambethia.com>
2012-01-25 23:33:57 -0500

    Gradient!

--- Triangle/shader.f.glsl
+++ Triangle/shader.f.glsl
@@ -4,5 +4,11 @@ out vec4 color;
 
 void main()
 {
-  color = vec4(1, 0, 0, 1);
+  float lerpValue = gl_FragCoord.y / 500.0f;
+
+  color = mix(
+    vec4(1.0f, 1.0f, 1.0f, 1.0f),
+    vec4(0.2f, 0.2f, 0.2f, 1.0f),
+    lerpValue
+  );
 }
